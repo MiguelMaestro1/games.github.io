6545be9879fb2e03c5dd2357e5eb6078791187c7
Miguel Bastos <miguel.bastos@hvc.nl>
2023-10-20 09:44:21 +0200

    Update H1O17.js

--- JS/H1/H1O17.js
+++ JS/H1/H1O17.js
@@ -7,17 +7,17 @@ function setup() {
 }
 
 function draw() {
-  background('cornflowerblue');
+  background('orange');
   schaal = 1 + mouseY / height;
-
+  tekenZon(mouseX,schaal);
   // teken de grond
   fill('wheat');
   rect(0,250,width,height - 250);  
 
+  tekenBoom(50);
+  tekenBoom(150);
+  tekenBoom(250);
   tekenHuis();
-
-  tekenBoom(700);
-  tekenBoom(900);
 }
 
 function tekenHuis() {
@@ -33,6 +33,16 @@ function tekenHuis() {
   pop();
 }
 
+function tekenBoom() {
+  push();
+  noStroke();
+  fill('sienna');
+  rect(x,130,40,130);
+  fill('olive');
+  ellipse(x + 20,130,100,150);
+  pop();
+}
+
 function tekenBoom(x) {
   push();
   noStroke();
@@ -42,3 +52,12 @@ function tekenBoom(x) {
   ellipse(x + 20,130,100,150);
   pop();
 }
+
+function tekenZon(x,s) {
+  push()
+  fill('red');
+  scale(s);
+  ellipse(x,200,300,300);
+  pop();
+ }
+ 
